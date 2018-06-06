9ddbfab33feb30bf16ff435e94ae1b73fc25cf0d
Aolia0 <1448276993@qq.com>
2018-06-06 22:07:03 +0800

    produceLogService

--- src/main/java/mapper/ProduceLogMapper.java
+++ src/main/java/mapper/ProduceLogMapper.java
@@ -2,6 +2,8 @@ package mapper;
 
 import po.ProduceLog;
 
+import java.util.List;
+
 public interface ProduceLogMapper {
     int deleteByPrimaryKey(Integer id);
 
@@ -10,6 +12,8 @@ public interface ProduceLogMapper {
     int insertSelective(ProduceLog record);
 
     ProduceLog selectByPrimaryKey(Integer id);
+
+    List<ProduceLog> selectByOrderKey(Integer oid, Integer process);
 
     int updateByPrimaryKeySelective(ProduceLog record);
 
